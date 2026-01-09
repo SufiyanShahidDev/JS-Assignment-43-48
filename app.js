e00868363c1b51a0359e9f3804c5bac5de145068
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-09 20:02:34 +0500

    Initial commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function resetImage(img) {
     img.src = "https://www.techspecs.info/_next/image/?url=https%3A%2F%2Fwww.techspecs.info%2Fuploads%2FSAMSUNG_GALAXY_S26_ULTRA_5_G_9232e2ad7e.png&w=256&q=75";
 }
 
-let counter = 0;
+let  counter = 0;
 
 function increase() {
     counter++;
