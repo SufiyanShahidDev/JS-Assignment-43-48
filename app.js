3a87db3f9c25bd7356094a36fa9db5901e54df16
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-09 19:50:35 +0500

    Initial commit

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ function deleteRow(button) {
     button.parentNode.parentNode.remove();
 }
 
-function changeImage(img) {
-    img.src = "https://via.placeholder.com/200?text=Image+2";
-}
+// function changeImage(img) {
+//     img.src = "https://images.priceoye.pk/apple-17-pro-max-pakistan-priceoye-5k9fi-500x500.webp";
+// }
 
-function resetImage(img) {
-    img.src = "https://www.techspecs.info/_next/image/?url=https%3A%2F%2Fwww.techspecs.info%2Fuploads%2FSAMSUNG_GALAXY_S26_ULTRA_5_G_9232e2ad7e.png&w=256&q=75";
-}
+// function resetImage(img) {
+//     img.src = "https://www.techspecs.info/_next/image/?url=https%3A%2F%2Fwww.techspecs.info%2Fuploads%2FSAMSUNG_GALAXY_S26_ULTRA_5_G_9232e2ad7e.png&w=256&q=75";
+// }
