a941cd6db414d679d568c3c0b5cae086d849bd30
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-09 19:48:59 +0500

    Initial commit

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ function showMessage() {
 function deleteRow(button) {
     button.parentNode.parentNode.remove();
 }
+
+function changeImage(img) {
+    img.src = "https://via.placeholder.com/200?text=Image+2";
+}
+
+function resetImage(img) {
+    img.src = "https://www.techspecs.info/_next/image/?url=https%3A%2F%2Fwww.techspecs.info%2Fuploads%2FSAMSUNG_GALAXY_S26_ULTRA_5_G_9232e2ad7e.png&w=256&q=75";
+}
