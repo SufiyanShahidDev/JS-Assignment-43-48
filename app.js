f3a2fd09a7acded4a5867d5030e54a25a65fca85
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-09 20:08:54 +0500

    Initial commit

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ function showAlert() {
     alert("Link was clicked!");
 };
 
+// Answer2
+
 function showMessage() {
     alert("Thanks for purchasing a mobile from us!");
 };
 
+// Answer3
+
 function deleteRow(button) {
     button.parentNode.parentNode.remove();
 };
+
+// Answer4
 
 function changeImage(img) {
     img.src = "https://images.priceoye.pk/apple-17-pro-max-pakistan-priceoye-5k9fi-500x500.webp";
@@ -20,6 +26,8 @@ function changeImage(img) {
 function resetImage(img) {
     img.src = "https://www.techspecs.info/_next/image/?url=https%3A%2F%2Fwww.techspecs.info%2Fuploads%2FSAMSUNG_GALAXY_S26_ULTRA_5_G_9232e2ad7e.png&w=256&q=75";
 };
+
+// Answer5
 
 let  counter = 0;
 
